d012d975b4bc5556b2029d2c80a0d90421a557d1
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-03 16:44:45 +0700

    Add IEnumerableHelper: ForEach<T>

---
+++ MrDHelper/IEnummerableHelper/IEnumerableHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrDHelper
+{
+    public static class IEnumerableHelper
+    {
+        public static void ForEach<T>(this IEnumerable<T>? enumeration, Action<T> action)
+        {
+            if (enumeration == null)
+            {
+                return;
+            }
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
