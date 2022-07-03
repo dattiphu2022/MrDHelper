92c831d66d976e15831b3ca9916b5b2fcc611134
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-03 16:45:13 +0700

    Add StringHelper: GetMd5

---
+++ MrDHelper/StringHelper/StringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrDHelper
+{
+    public static class StringHelper
+    {
+        public static string GetMd5(this string input)
+        {
+            using var provider = System.Security.Cryptography.MD5.Create();
+            StringBuilder builder = new StringBuilder();
+
+            foreach (byte b in provider.ComputeHash(Encoding.UTF8.GetBytes(input)))
+                builder.Append(b.ToString("x2").ToLower());
+
+            return builder.ToString();
+        }
+    }
+}
