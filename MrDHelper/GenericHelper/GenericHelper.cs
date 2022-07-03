6f92cfa2a81aac7bb306ea187bcee6de2cdde75a
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-03 16:44:20 +0700

    Add GenericHelper: IsNull, NotNull

---
+++ MrDHelper/GenericHelper/GenericHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrDHelper
+{
+    public static class GenericHelper
+    {
+        public static bool IsNull<T>(this T input)
+        {
+            return input is null;
+        }
+        public static bool NotNull<T>(this T input)
+        {
+            return !IsNull(input);
+        }
+    }
+}
