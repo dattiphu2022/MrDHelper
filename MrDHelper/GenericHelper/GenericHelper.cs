9a7d743e0443599f3fcc8c2751b432a28edc34de
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-03 22:24:26 +0700

    update to add comments

--- MrDHelper/GenericHelper/GenericHelper.cs
+++ MrDHelper/GenericHelper/GenericHelper.cs
@@ -4,12 +4,28 @@ using System.Text;
 
 namespace MrDHelper
 {
+    /// <summary>
+    /// Extension methods for generic usage.
+    /// </summary>
     public static class GenericHelper
     {
+        /// <summary>
+        /// Check if the object is null
+        /// </summary>
+        /// <typeparam name="T">Input</typeparam>
+        /// <param name="input">object to check</param>
+        /// <returns>True if T is null, and vice versa.</returns>
         public static bool IsNull<T>(this T input)
         {
             return input is null;
         }
+
+        /// <summary>
+        /// Check if the object is not null
+        /// </summary>
+        /// <typeparam name="T">Input</typeparam>
+        /// <param name="input">object to check</param>
+        /// <returns>False if T is null, and vice versa.</returns>
         public static bool NotNull<T>(this T input)
         {
             return !IsNull(input);
--- MrDHelper/StringHelper/StringHelper.cs
+++ MrDHelper/StringHelper/StringHelper.cs
@@ -4,10 +4,19 @@ using System.Text;
 
 namespace MrDHelper
 {
+    /// <summary>
+    /// String extension methods.
+    /// </summary>
     public static class StringHelper
     {
-        public static string GetMd5(this string input)
+        /// <summary>
+        /// Get MD5 hash from a string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Null if <paramref name="input"/> is null; MD5 if <paramref name="input"/> is not null.</returns>
+        public static string? GetMd5(this string? input)
         {
+            if (input == null) return null;
             using var provider = System.Security.Cryptography.MD5.Create();
             StringBuilder builder = new StringBuilder();
 
