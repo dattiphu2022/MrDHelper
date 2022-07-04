a9f14a3e631a797d22c8ff9e53a38fb90e2f61f2
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-04 08:46:24 +0700

    add IEnumerableHelper

---
+++ MrDHelper/CollectionHelpers/IEnummerableHelper/IEnumerableHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrDHelper
+{
+    /// <summary>
+    /// Extension methods for <see cref="IEnumerable{T}"/>
+    /// </summary>
+    public static class IEnumerableHelper
+    {
+        /// <summary>
+        /// Apply <paramref name="action"/> to every item in <paramref name="enumeration"/>.
+        /// Skip ForEach if <paramref name="enumeration"/> is <see cref="null"/>
+        /// </summary>
+        /// <typeparam name="T">object item</typeparam>
+        /// <param name="enumeration">IEnummerable<typeparamref name="T"/></param>
+        /// <param name="action"></param>
+        public static void ForEach<T>(this IEnumerable<T>? enumeration, Action<T> action)
+        {
+            if (enumeration == null)
+            {
+                return;
+            }
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
