98af930a947ba8387ddce2e9abbbe30dd400743b
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-04 08:46:34 +0700

    Add IListHelper

---
+++ MrDHelper/CollectionHelpers/IListHelper/IListHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MrDHelper
+{
+    /// <summary>
+    /// <see cref="IList{T}"/> extension methods.
+    /// </summary>
+    public static class IListHelper
+    {
+        /// <summary>
+        /// Add dummy items to the <see cref="IList{T}"/> collection to increase collection count = <paramref name="collectionFinalCount"/>
+        /// with the fillin value is <paramref name="fillValue"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="collectionFinalCount"></param>
+        /// <param name="fillValue"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void AddDummyItemsToMaximumCountOf<T>(this IList<T> collection,
+            int collectionFinalCount, T fillValue)
+        {
+            if (collection.IsNull())
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            if (collectionFinalCount < collection.Count)
+            {
+                throw new InvalidOperationException($"{nameof(collectionFinalCount)}.Value can't < {nameof(collection)}.Count");
+            }
+
+            var itemCountToAdd = collectionFinalCount - collection.Count;
+
+            //dummyCollection is not null here
+            var dummyCollection = new T[itemCountToAdd];
+            Array.Fill(dummyCollection, fillValue);
+            Array.ForEach(dummyCollection, (dummyItem) =>
+            {
+                collection.Add(dummyItem);
+            });
+        }
+    }
+}
