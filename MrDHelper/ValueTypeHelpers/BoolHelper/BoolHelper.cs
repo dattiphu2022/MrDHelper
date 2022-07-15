16c42a0462270c05d8b9215b1f885d56ea798d55
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-15 16:21:15 +0700

    add BoolHelper (this method has no tests)

---
+++ MrDHelper/ValueTypeHelpers/BoolHelper/BoolHelper.cs
@@ -0,0 +1,92 @@
+﻿namespace MrDHelper
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+
+    /// <summary>
+    /// Bool extension methods.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public static class BoolHelper
+    {
+        #region Method start with IS
+        /// <summary>
+        /// Short way of "bool? == false"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool IsFalse(this bool? input)
+        {
+            return input == false;
+        }
+        /// <summary>
+        /// Short way of "bool == false"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool IsFalse(this bool input)
+        {
+            return input == false;
+        }
+
+        /// <summary>
+        /// Short way of "bool? == true"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool IsTrue(this bool? input)
+        {
+            return input == true;
+        }
+        /// <summary>
+        /// Short way of "bool == true"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool IsTrue(this bool input)
+        {
+            return input == true;
+        }
+        #endregion
+        #region Method start with NOT
+        /// <summary>
+        /// Short way of "bool? != false"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool NotFalse(this bool? input)
+        {
+            return input != false;
+        }
+        /// <summary>
+        /// Short way of "bool != false"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool NotFalse(this bool input)
+        {
+            return input != false;
+        }
+
+        /// <summary>
+        /// Short way of "bool? != true"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool NotTrue(this bool? input)
+        {
+            return input != true;
+        }
+        /// <summary>
+        /// Short way of "bool != true"
+        /// </summary>
+        /// <param name="input">bool to check</param>
+        /// <returns><see cref="Boolean"/></returns>
+        public static bool NotTrue(this bool input)
+        {
+            return input != true;
+        }
+        #endregion
+    }
+}
