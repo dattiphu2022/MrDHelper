3a1310c1690f9997f749734e85dda9c7f8767188
Nguyen Quoc Dat <dattiphu2022@gmail.com>
2022-07-17 21:58:52 +0700

    Adding Cell and CellExtension

---
+++ MrDHelper/Extensions/CellExtension.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace MrDHelper
+{
+    public static class CellExtension
+    {
+        public static T? ConvertTo<T>(this Cell source) where T : class, new()
+        {
+            if (source.IsNull())
+            {
+                return null;
+            }
+            var ouput = new T();
+            var properties = ouput?.GetType().GetProperties();
+            if (properties.NotNull())
+            {
+                properties.ForEach(x =>
+                {
+                    x.SetValue(ouput, source[x.Name]);
+                });
+            }
+            return ouput;
+        }
+        public static Cell? ConvertToCell<T>(this T? source) where T : class
+        {
+            if (source.IsNull())
+            {
+                return null;
+            }
+            Cell? output = new Cell();
+
+            output.Datas = source.GetProperties();
+
+            return output;
+        }
+        public static Dictionary<string, object?> GetProperties<T>(this T source)
+        {
+            Dictionary<string, object?> output = new Dictionary<string, object?>();
+
+            if (source.IsNull())
+            {
+                return output;
+            }
+            var properties = source?.GetType().GetProperties();
+            if (properties.NotNull())
+            {
+                properties.ForEach(x =>
+                {
+                    output.Add(x.Name, x.GetValue(source));
+                });
+            }
+            return output;
+        }
+    }
+}
