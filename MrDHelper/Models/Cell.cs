36fd769012d21c1457267008eca4d2c44d6affb5
Game Master <friendlyhospital@gmail.com>
2022-10-14 14:55:51 +0700

    update Cell model to sealed class

--- MrDHelper/Models/Cell.cs
+++ MrDHelper/Models/Cell.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    public class Cell : IDisposable
+    public sealed class Cell : IDisposable
     {
         public object? this[string propertyName]
         {
