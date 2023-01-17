662b4e0c9e0c4741b1691db169c0ef850fe044e4
Game Master <friendlyhospital@gmail.com>
2023-01-17 20:11:34 +0700

    add User32.cs

---
+++ MrDHelper/DllImport/User32.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MrDHelper.DllImport
+{
+    public class User32
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+    }
+}
